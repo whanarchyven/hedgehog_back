2141feb0ade3ac41d1d0e9f633774c9954849bee
Ivan Zaburdaev <ivanlom227@gmail.com>
2023-07-21 15:48:47 +0300

    try to fix

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -5,194 +5,206 @@ import fileService from "../services/fileService.js";
 import posts from "../routes/posts.js";
 
 class PostController {
-    //get all posts
-    async getAll(req, res) {
-        Posts.find()
-            .sort({date: -1})
-            .then((posts) => res.json(posts))
-            .catch((err) => res.status(400).json("error: " + err));
+  //get all posts
+  async getAll(req, res) {
+    Posts.find()
+      .sort({ date: -1 })
+      .then((posts) => res.json(posts))
+      .catch((err) => res.status(400).json("error: " + err));
+  }
+
+  //add new post
+  async create(req, res) {
+    const { caption, location, xCoord, yCoord } = req.body;
+    const fileName = fileService.saveFile(req.files.image);
+    console.log(req.user);
+    let nick = "";
+    User.findById(req.user.id).then((user, err) => {
+      if (user) {
+        nick = `${user?.name} ${user?.surname}`;
+      }
+      console.log(nick);
+      const newPost = new Posts({
+        caption,
+        image: fileName,
+        location,
+        xCoord,
+        yCoord,
+        username: req.user.username,
+        nickname: nick,
+      });
+      newPost
+        .save()
+        .then(() => res.json("Post Added"))
+        .catch((err) => res.status(400).json(err));
+    });
+  }
+
+  //update post
+  async updatePost(req, res) {
+    try {
+      const post = req.body;
+      if (!post._id) {
+        res.status(400).json({ message: "Id не указан" });
+      }
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {
+        new: true,
+      });
+      return res.status(500).json(post);
+    } catch (err) {
+      return res.status(400).json({ message: "Error" });
     }
+  }
 
-    //add new post
-    async create(req, res) {
-        const {caption, location, xCoord, yCoord} = req.body;
-        const fileName = fileService.saveFile(req.files.image)
-        console.log(req.user)
-        let nick=''
-        User.findById(req.user.id).then((user,err)=>{
-            nick=`${user?.name} ${user?.surname}`
-            console.log(nick)
-            const newPost = new Posts({
-                caption,
-                image: fileName, location, xCoord, yCoord,
-                username: req.user.username,
-                nickname:nick,
-            });
-            newPost
-                .save()
-                .then(() => res.json("Post Added"))
-                .catch((err) => res.status(400).json(err));
-        })
+  //get post by id
+  async getPost(req, res) {
+    Posts.findById(req.params.id).then((post, err) => {
+      if (err) res.status(400).json("error: " + err);
+      else res.status(200).json(post);
+    });
+  }
 
+  //delete post
+  async deletePost(req, res) {
+    try {
+      const { postId } = req.params.id;
+      const post = await Post.findByIdAndDelete(postId);
+      return res.status(500).json(post);
+    } catch (err) {
+      return res.status(400).json({ message: "Error" });
     }
+  }
 
-    //update post
-    async updatePost(req, res) {
-        try {
-            const post = req.body
-            if (!post._id) {
-                res.status(400).json({message: "Id не указан"})
+  async getComments(req, res) {
+    Posts.findById(req.params.id).then((post, err) => {
+      if (err) res.status(400).json("error: " + err);
+      else res.status(200).json(post.comments);
+    });
+  }
+
+  async getUserPosts(req, res) {
+    Posts.find({ username: req.params.username }).then((posts, err) => {
+      if (err) res.status(400).json("error: " + err);
+      else res.status(200).json(posts);
+    });
+  }
+
+  //add new comment
+  async addComment(req, res) {
+    const { content, id } = req.body;
+    const user = req.user;
+    console.log(req.user);
+
+    let nick = "";
+    User.findById(req.user.id).then((user, err) => {
+      nick = `${user?.name} ${user?.surname}`;
+      console.log(nick);
+      const newContent = new Comment({
+        content,
+        username: user.username,
+        nickname: nick,
+      });
+      newContent.save().then(() => res.json("Comment Added"));
+      Posts.findByIdAndUpdate(
+        { _id: id },
+        { $push: { comments: newContent } }
+      ).then((err, data) => {
+        // if (err) res.status(400).json("error suka: " + err);
+        // else res.status(200).json(data);
+      });
+    });
+  }
+
+  async addLike(req, res) {
+    const { id } = req.body;
+    Posts.findById({ _id: id }).then((data, err) => {
+      if (err) res.status(400).json("error: " + err);
+      else {
+        let userLike = data.likes.findIndex((item) => item == req.user.id);
+
+        if (userLike == -1) {
+          let userDislike = data.dislikes.findIndex(
+            (item) => item == req.user.id
+          );
+
+          if (userDislike != -1) {
+            let temp = data.dislikes.splice(userDislike, 1);
+            if (data.likes.length == 0) {
+              temp = [];
             }
-            const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true})
-            return res.status(500).json(post)
-        } catch (err) {
-            return res.status(400).json({message: 'Error'})
-        }
-    }
-
-    //get post by id
-    async getPost(req, res) {
-        Posts.findById(req.params.id).then((post, err) => {
-            if (err) res.status(400).json("error: " + err);
-            else res.status(200).json(post);
-        });
-    }
-
-    //delete post
-    async deletePost(req, res) {
-        try {
-            const {postId} = req.params.id
-            const post = await Post.findByIdAndDelete(postId);
-            return res.status(500).json(post)
-        } catch (err) {
-            return res.status(400).json({message: 'Error'})
-        }
-    }
-
-
-    async getComments(req, res) {
-        Posts.findById(req.params.id).then((post, err) => {
-            if (err) res.status(400).json("error: " + err);
-            else res.status(200).json(post.comments);
-        });
-    }
-
-    async getUserPosts(req,res){
-        Posts.find({username:req.params.username}).then((posts,err)=>{
-            if (err) res.status(400).json("error: " + err);
-            else res.status(200).json(posts);
-        })
-    }
-
-
-    //add new comment
-    async addComment(req, res) {
-        const {content, id} = req.body;
-        const user = req.user;
-        console.log(req.user)
-
-        let nick=''
-        User.findById(req.user.id).then((user,err)=>{
-            nick=`${user?.name} ${user?.surname}`
-            console.log(nick)
-            const newContent = new Comment({
-                content,
-                username: user.username,
-                nickname:nick,
-            });
-            newContent.save().then(() => res.json("Comment Added"));
             Posts.findByIdAndUpdate(
-                {_id: id},
-                {$push: {comments: newContent}}).then((err, data) => {
-                    // if (err) res.status(400).json("error suka: " + err);
-                    // else res.status(200).json(data);
-                }
-            );
-        })
-    }
+              { _id: id },
+              { $set: { dislikes: temp } }
+            ).then(() => {});
+          }
 
-    async addLike(req,res){
-        const {id}=req.body;
-        Posts.findById({_id:id}).then((data,err)=>{
-            if (err) res.status(400).json("error: " + err);
-            else {
-                let userLike=data.likes.findIndex(item=>item==req.user.id)
-
-                if(userLike==-1){
-
-                    let userDislike=data.dislikes.findIndex(item=>item==req.user.id)
-
-                    if(userDislike!=-1){
-                        let temp=data.dislikes.splice(userDislike,1)
-                        if(data.likes.length==0){temp=[]}
-                        Posts.findByIdAndUpdate({_id: id},
-                            {$set: {dislikes:temp}}).then(()=>{
-                        })
-                    }
-
-                    Posts.findByIdAndUpdate({_id: id},
-                        {$push: {likes:req.user.id}}).then(()=>{
-                        return res.status(200).json("like added")
-                    })
-                }
-                else{
-                    let temp=data.likes.splice(userLike,1)
-                    if(data.likes.length==0){temp=[]}
-                    console.log(temp)
-                    Posts.findByIdAndUpdate({_id: id},
-                        {$set: {likes:temp}}).then(()=>{
-                        return res.status(200).json("like removed")
-                    })
-                }
+          Posts.findByIdAndUpdate(
+            { _id: id },
+            { $push: { likes: req.user.id } }
+          ).then(() => {
+            return res.status(200).json("like added");
+          });
+        } else {
+          let temp = data.likes.splice(userLike, 1);
+          if (data.likes.length == 0) {
+            temp = [];
+          }
+          console.log(temp);
+          Posts.findByIdAndUpdate({ _id: id }, { $set: { likes: temp } }).then(
+            () => {
+              return res.status(200).json("like removed");
             }
-        })
-
-    }
-
-    async addDislike(req,res){
-        try{
-            const {id}=req.body;
-            Posts.findById({_id:id}).then((data,err)=>{
-                if (err) res.status(400).json("error: " + err);
-                else {
-                    let userLike=data.dislikes.findIndex(item=>item==req.user.id)
-
-                    if(userLike==-1){
-
-                        let userLike=data.likes.findIndex(item=>item==req.user.id)
-
-                        if(userLike!=-1){
-                            let temp=data.likes.splice(userLike,1)
-                            if(data.likes.length==0){temp=[]}
-                            Posts.findByIdAndUpdate({_id: id},
-                                {$set: {likes:temp}}).then(()=>{
-                            })
-                        }
-
-
-                        Posts.findByIdAndUpdate({_id: id},
-                            {$push: {dislikes:req.user.id}}).then(()=>{
-                            return res.status(200).json("dislike added")
-                        })
-                    }
-                    else{
-                        let temp=data.dislikes.splice(userLike,1)
-                        if(data.dislikes.length==0){temp=[]}
-                        console.log(temp)
-                        Posts.findByIdAndUpdate({_id: id},
-                            {$set: {dislikes:temp}}).then(()=>{
-                            return res.status(200).json("dislike removed")
-                        })
-                    }
-                }
-            })
-            
+          );
         }
-        catch (e) {
+      }
+    });
+  }
 
+  async addDislike(req, res) {
+    try {
+      const { id } = req.body;
+      Posts.findById({ _id: id }).then((data, err) => {
+        if (err) res.status(400).json("error: " + err);
+        else {
+          let userLike = data.dislikes.findIndex((item) => item == req.user.id);
+
+          if (userLike == -1) {
+            let userLike = data.likes.findIndex((item) => item == req.user.id);
+
+            if (userLike != -1) {
+              let temp = data.likes.splice(userLike, 1);
+              if (data.likes.length == 0) {
+                temp = [];
+              }
+              Posts.findByIdAndUpdate(
+                { _id: id },
+                { $set: { likes: temp } }
+              ).then(() => {});
+            }
+
+            Posts.findByIdAndUpdate(
+              { _id: id },
+              { $push: { dislikes: req.user.id } }
+            ).then(() => {
+              return res.status(200).json("dislike added");
+            });
+          } else {
+            let temp = data.dislikes.splice(userLike, 1);
+            if (data.dislikes.length == 0) {
+              temp = [];
+            }
+            console.log(temp);
+            Posts.findByIdAndUpdate(
+              { _id: id },
+              { $set: { dislikes: temp } }
+            ).then(() => {
+              return res.status(200).json("dislike removed");
+            });
+          }
         }
-    }
-
+      });
+    } catch (e) {}
+  }
 }
 
 export default new PostController();
