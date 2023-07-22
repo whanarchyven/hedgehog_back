bce1950b27b45356b63f9aa11f45367ff29e6c08
Ivan Zaburdaev <ivanlom227@gmail.com>
2023-07-22 12:18:34 +0300

    trying to add ssl

--- server.js
+++ server.js
@@ -1,33 +1,44 @@
-import express from "express"
-import mongoose from "mongoose"
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import fileUpload from "express-fileupload"
+import fileUpload from "express-fileupload";
 
 import postsRouter from "./routes/posts.js";
 import userRouter from "./routes/users.js";
 import commentRouter from "./routes/comment.js";
 
+import * as fs from "fs";
+import * as https from "https";
+
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 app.use(cors());
 app.use(express.json());
-app.use(fileUpload({}))
+app.use(fileUpload({}));
 
-const uri = 'mongodb+srv://ivanlom227:Whaven1488@whereishedgehog.qvq7vwc.mongodb.net/?retryWrites=true&w=majority';
+const options = {
+  cert: fs.readFileSync("./sslcert/fullchain.pem"),
+  key: fs.readFileSync("./sslcert/privkey.pem"),
+};
+
+const uri =
+  "mongodb+srv://ivanlom227:Whaven1488@whereishedgehog.qvq7vwc.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
-    console.log("Databse connection established");
+  console.log("Databse connection established");
 });
 
-app.use('/posts',postsRouter)
-app.use('/users',userRouter)
-app.use('/comments',commentRouter)
+app.use("/posts", postsRouter);
+app.use("/users", userRouter);
+app.use("/comments", commentRouter);
 // app.use('/static', express.static('public'));
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
+
+https.createServer(options, app).listen(443);
